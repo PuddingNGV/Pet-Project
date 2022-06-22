983b8d37943edc1e93f5d946d2d828d93dfa52e5
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-06-22 15:52:52 +0300

    All replaced by View Binding

--- feature-main-screen/src/main/java/com/example/feature_main_screen/presentation/StartMainScreen.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/presentation/StartMainScreen.kt
@@ -2,6 +2,7 @@ package com.example.feature_main_screen.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.feature_main_screen.R
 import com.example.feature_main_screen.databinding.ActivityMainScreenBinding
+import com.example.feature_main_screen.databinding.StageInfoLayoutBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.squareup.picasso.Picasso
 
@@ -17,6 +19,7 @@ import com.squareup.picasso.Picasso
 class StartMainScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainScreenBinding
+    private lateinit var bindingStage: StageInfoLayoutBinding
 
     //private val rocketRepo by lazy { RocketRepoImpl(applicationContext) }
     //private val getDataUseCase by lazy { GetDataUseCase(rocketRepo) }
@@ -53,25 +56,21 @@ class StartMainScreen : AppCompatActivity() {
                 getString(R.string.cost_per_launch_val, ((it.costPerLaunch) / 1000000))
 
             fun addStageField(view: View, nextStage: Int) {
-                val stageLayoutInflater = layoutInflater.inflate(R.layout.stage_info_layout, null)
-                stageLayoutInflater.findViewById<TextView>(R.id.textStage).text = getString(
+                bindingStage = StageInfoLayoutBinding.bind(view)
+                bindingStage.textStage.text = getString(
                     R.string.stage_name,
                     resources.getStringArray(R.array.number_stage)[nextStage]
                 )
-                stageLayoutInflater.findViewById<TextView>(R.id.textEnginesVal).text =
-                    it.stageInfo[nextStage].engines.toString()
-                stageLayoutInflater.findViewById<TextView>(R.id.textFuelAmountTonsVal).text =
-                    it.stageInfo[nextStage].fuelAmountTons.toString()
-                stageLayoutInflater.findViewById<TextView>(R.id.textBurnTimeSecVal).text =
-                    it.stageInfo[nextStage].burnTimeSec.toString()
-
-                binding.bottomSheetInclude.linearLayout.addView(stageLayoutInflater)
+                bindingStage.textEnginesVal.text = it.stageInfo[nextStage].engines.toString()
+                bindingStage.textFuelAmountTonsVal.text = it.stageInfo[nextStage].fuelAmountTons.toString()
+                bindingStage.textBurnTimeSecVal.text = it.stageInfo[nextStage].burnTimeSec.toString()
+                binding.bottomSheetInclude.linearLayout.addView(view)
             }
 
             for (nextStage in 0 until it.stageCount) {
-                addStageField(view, nextStage)
+                val stageLayoutInflater = layoutInflater.inflate(R.layout.stage_info_layout, null, false)
+                addStageField(stageLayoutInflater, nextStage)
             }
-
             val buttonLayoutInflater = layoutInflater.inflate(R.layout.button_launch, null)
             binding.bottomSheetInclude.linearLayout.addView(buttonLayoutInflater)
         })
