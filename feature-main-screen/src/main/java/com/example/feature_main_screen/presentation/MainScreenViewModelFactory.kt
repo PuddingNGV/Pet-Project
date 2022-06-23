5cc596a91d761432c4c12d0e22a23ec27ed1c199
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-06-23 14:05:32 +0300

    Disabled ViewModelFactory
    class ViewModelFactory is deprecated

--- feature-main-screen/src/main/java/com/example/feature_main_screen/presentation/MainScreenViewModelFactory.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/presentation/MainScreenViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.feature_main_screen.data.remote.RocketRepoImpl
 import com.example.feature_main_screen.domain.usecase.GetDataUseCase
-
+/*
 class MainScreenViewModelFactory(context: Context) : ViewModelProvider.Factory {
 
     private val rocketRepo by lazy { RocketRepoImpl(context) }
@@ -17,3 +17,5 @@ class MainScreenViewModelFactory(context: Context) : ViewModelProvider.Factory {
         ) as T
     }
 }
+
+ */
