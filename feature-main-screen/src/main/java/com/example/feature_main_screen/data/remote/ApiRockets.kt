2b932b9f5939dd6675abe09a7dfc00756d7ee1c9
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-06-24 12:29:38 +0300

    Deleted const URL
    not used here

--- feature-main-screen/src/main/java/com/example/feature_main_screen/data/remote/ApiRockets.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/data/remote/ApiRockets.kt
@@ -4,9 +4,6 @@ import com.example.feature_main_screen.data.remote.responce.RocketResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
-const val baseUrl = "https://api.spacexdata.com/"
-
-
 interface ApiRockets {
 
     @GET("./v4/rockets")
