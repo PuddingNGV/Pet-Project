528bfe1c4f4f90428b93c4384e779473c6418681
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-07-15 14:55:15 +0300

    Updated entity for tible room

--- feature-main-screen/src/main/java/com/example/feature_main_screen/data/local/entity/RocketDbEntity.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/data/local/entity/RocketDbEntity.kt
@@ -1,7 +1,10 @@
 package com.example.feature_main_screen.data.local.entity
 
 import androidx.room.*
-import com.example.feature_main_screen.data.local.entity.engines.Engines
+import com.example.feature_main_screen.data.local.entity.engines.EnginesLocal
+import com.example.feature_main_screen.domain.models.RocketInfo
+import com.example.feature_main_screen.domain.models.StageInfo
+import java.util.List.copyOf
 
 @Entity (
     tableName = "rockets",
@@ -21,15 +24,19 @@ data class RocketDbEntity(
     val type: String,
     @ColumnInfo(name = "first_flight_") val firstFlight: String,
     @ColumnInfo(name = "cost_per_launch_") val costPerLaunch: Int,
-    @Embedded(prefix = "engines_") val engines: Engines,
-    @Embedded(prefix = "diameter_") val diameter: Diameter,
-    @Embedded(prefix = "height_") val height: Height,
-    @Embedded(prefix = "mass_") val mass: Mass,
-    @Embedded(prefix = "landingLegs_") val landingLegs: LandingLegs,
+    @Embedded(prefix = "engines_") val engines: EnginesLocal,
+    @Embedded(prefix = "diameter_") val diameter: DiameterLocal,
+    @Embedded(prefix = "height_") val height: HeightLocal,
+    @Embedded(prefix = "mass_") val mass: MassLocal,
+    @Embedded(prefix = "landingLegs_") val landingLegs: LandingLegsLocal,
     val description: String,
     val wikipedia: String,
 
-    ) /*{
+    val stage: List<StageLocal>,
+    val payloadWeights: List<PayloadWeightLocal>,
+    val flickrImage: List<String>
+    ) {
+
     fun toRocketInfo(heightType:Boolean, diameterType:Boolean, massType:Boolean, payloadType:Boolean):RocketInfo = RocketInfo(
         rocketName = name,
         height = when (heightType) {
@@ -45,95 +52,30 @@ data class RocketDbEntity(
             false -> mass.lb
         },
         payload = when (payloadType) {
-            true -> payloadWeights.kg
-            false -> payloadWeights.lb
+            true -> payloadWeights[0].kg
+            false -> payloadWeights[0].lb
         },
-        imageUlrList = List<String>,
+        imageUlrList = flickrImage,
         firstFlight = firstFlight,
         country = country,
         costPerLaunch = costPerLaunch,
         stageCount = stages,
-        stageInfo = List<stage>,
+        stageInfo = toListStageInfo(stage),
     )
 }
-*/
 
 
+fun toStageInfo(stage: StageLocal):StageInfo = StageInfo(
+    engines = stage.engines,
+    fuelAmountTons = stage.fuelAmountTons,
+    burnTimeSec = stage.burnTimeSec)
+
+fun toListStageInfo(stage: List<StageLocal>):List<StageInfo> {
+    val mListStageInfo = mutableListOf<StageInfo>()
+    for (i in stage.indices){
+        mListStageInfo.add(toStageInfo(stage[i]))
+    }
+    return copyOf(mListStageInfo)
+}
 
 
-
-@Entity (
-    tableName = "stage",
-    indices = [
-        Index("id_stage", unique = true)
-    ]
-)
-data class StageDbEntity(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id_stage")val idStage: Int,
-    @ColumnInfo(name = "burn_time_sec") val burnTimeSec: Int,
-    val engines: Int,
-    @ColumnInfo(name = "fuel_amount_tons") val fuelAmountTons: Double,
-    val reusable: Boolean
-)
-
-data class RocketWithStage(
-    @Embedded val rocketDbEntity: RocketDbEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id_stage"
-    )
-    val stageList:List<StageDbEntity>
-)
-
-
-
-
-
-@Entity (
-    tableName = "image",
-    indices = [
-        Index("id_image", unique = true)
-    ]
-)
-data class FlickrImageDbEntity(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id_image") val idImage:Int,
-    val url:String
-)
-
-data class RocketWithFlickrImage(
-    @Embedded val rocketDbEntity: RocketDbEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id_image"
-    )
-    val stageList:List<FlickrImageDbEntity>
-)
-
-
-
-
-
-
-
-
-@Entity (
-    tableName = "payload_weight",
-    indices = [
-        Index("payload_id", unique = true)
-    ]
-)
-data class PayloadWeightDbEntity(
-    @PrimaryKey(autoGenerate = false) @ColumnInfo(name = "payload_id")val id: String,
-    val name: String,
-    val kg: Int,
-    val lb: Int
-)
-
-data class RocketWithPayloadWeight(
-    @Embedded val rocketDbEntity: RocketDbEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "payload_id"
-    )
-    val stageList:List<PayloadWeightDbEntity>
-)
