979ed0312b7db8c765906a1bc42b301a531edf19
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-07-15 14:54:24 +0300

    Added module room for di

---
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/di/DatabaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.feature_main_screen.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import com.example.feature_main_screen.data.local.AppRocketDataBase
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ProductionModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(app: Application): AppRocketDataBase =
+        Room.databaseBuilder(app, AppRocketDataBase::class.java, "rocket_database")
+            .build()
+
+}
