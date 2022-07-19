32a38b6e660f990da84840745f65dc5ab0f251bd
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-07-19 12:09:30 +0300

    Deleted unused imports

--- feature-main-screen/src/main/java/com/example/feature_main_screen/domain/usecase/GetDataUseCase.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/domain/usecase/GetDataUseCase.kt
@@ -1,14 +1,12 @@
 package com.example.feature_main_screen.domain.usecase
 
 
-import androidx.lifecycle.asLiveData
 import com.example.feature_main_screen.data.local.entity.RocketDbEntity
 import com.example.feature_main_screen.domain.models.RocketInfo
 import com.example.feature_main_screen.domain.repository.RocketRepo
 import com.example.feature_main_screen.until.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.withContext
 
 class GetDataUseCase(private val rocketRepo: RocketRepo) {
