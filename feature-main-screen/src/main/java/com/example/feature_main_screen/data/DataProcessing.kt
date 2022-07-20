0f4c2e0b70895244fb17b5afe0bdc7f063cce4e1
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-07-20 14:26:48 +0300

    Added flow type converter

--- feature-main-screen/src/main/java/com/example/feature_main_screen/data/DataProcessing.kt
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/data/DataProcessing.kt
@@ -5,9 +5,10 @@ import com.example.feature_main_screen.data.remote.responce.item.RocketResponseI
 import com.example.feature_main_screen.data.remote.responce.item.stage.Stage
 import com.example.feature_main_screen.domain.models.RocketInfo
 import com.example.feature_main_screen.domain.models.StageInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class DataProcessing {
-
     fun toLocalListRocketDbEntity(rocketResponseList: List<RocketResponseItem>): List<RocketDbEntity> {
         val mEntityList = mutableListOf<RocketDbEntity>()
         for (i in rocketResponseList.indices){
@@ -15,8 +16,20 @@ class DataProcessing {
         }
         return java.util.List.copyOf(mEntityList)
     }
-
-   fun rocketProcessing(
+    fun toRocketInfo(listFlow: Flow<List<RocketDbEntity>>): Flow<List<RocketInfo>> {
+        val rocketInfoList: Flow<List<RocketInfo>> = listFlow.map { list ->
+            list.map { value ->
+                value.toRocketInfo(true,true,true,true)
+            }
+        }
+        return rocketInfoList
+    }
+    private fun toRocketInfoDeprecated(rocketDbEntity: List<RocketDbEntity>): List<RocketInfo> {
+        val mEntityList = mutableListOf<RocketInfo>()
+        rocketDbEntity.forEach{ value -> mEntityList.add(value.toRocketInfo(true,true,true,true))}
+        return java.util.List.copyOf(mEntityList)
+    }
+    fun rocketProcessing(
         request: RocketResponseItem,
         param: Map<String, Boolean>
     ): RocketInfo {
