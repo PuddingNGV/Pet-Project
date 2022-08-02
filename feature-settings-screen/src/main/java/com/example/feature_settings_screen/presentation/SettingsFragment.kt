b3241695d7c03d7db647a2f94f0df588692a872c
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-02 13:06:43 +0300

    Create SettingsFragment.kt

---
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.feature_settings_screen.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import com.example.feature_settings_screen.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private val vm: SettingsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
