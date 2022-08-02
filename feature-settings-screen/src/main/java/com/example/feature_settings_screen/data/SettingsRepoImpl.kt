4a272115e8d5ae6c7ed8527ea1650be78a379f14
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-02 13:06:25 +0300

    Create SettingsRepoImpl.kt

---
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/data/SettingsRepoImpl.kt
@@ -0,0 +1,16 @@
+package com.example.feature_settings_screen.data
+
+import javax.inject.Singleton
+import com.example.feature_settings_screen.domain.repository.SettingsRepo
+
+@Singleton
+class SettingsRepoImpl() : SettingsRepo {
+    override fun safeSettings() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSettings() {
+        TODO("Not yet implemented")
+    }
+
+}
