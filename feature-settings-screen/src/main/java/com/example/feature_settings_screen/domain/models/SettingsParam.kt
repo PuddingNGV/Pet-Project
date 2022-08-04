1ea3c1fcabd42091c9fcf2a1224b0856bf392f6e
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-04 15:21:19 +0300

    Create models SettingsParam

---
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/domain/models/SettingsParam.kt
@@ -0,0 +1,8 @@
+package com.example.feature_settings_screen.domain.models
+
+data class SettingsParam(
+    val heightParam:Boolean,
+    val diameterParam:Boolean,
+    val massParam:Boolean,
+    val payloadParam:Boolean
+    )
