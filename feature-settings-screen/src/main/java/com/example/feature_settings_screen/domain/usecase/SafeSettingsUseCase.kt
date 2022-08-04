e29d4b543096be8fec4471291dcfd96a3a72bd58
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-04 15:22:35 +0300

    Update SafeSettingsUseCase.kt

--- feature-settings-screen/src/main/java/com/example/feature_settings_screen/domain/usecase/SafeSettingsUseCase.kt
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/domain/usecase/SafeSettingsUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.feature_settings_screen.domain.usecase
 
 import com.example.feature_settings_screen.domain.repository.SettingsRepo
+import com.example.feature_settings_screen.domain.models.SettingsParam
 
 class SafeSettingsUseCase(private val settingsRepo: SettingsRepo) {
-    fun execute() = settingsRepo.safeSettings()
+    fun execute(settingsParam: SettingsParam) = settingsRepo.safeSettings(settingsParam)
 }
