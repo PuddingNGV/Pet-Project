ebcd0f9f7271a230b75117ee3729a6a00ddfb000
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-04 15:26:06 +0300

    Update SettingsViewModel.kt

--- feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsViewModel.kt
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import com.example.feature_settings_screen.domain.usecase.*
+import com.example.feature_settings_screen.domain.models.SettingsParam
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(private val getSettingsUseCase: GetSettingsUseCase, private val safeSettingsUseCase: SafeSettingsUseCase) : ViewModel() {
-    init {
+
+    fun safe() {
+        val settingsParam = SettingsParam(false,false,false,false)
+        safeSettingsUseCase.execute(settingsParam)
+    }
+
+    fun load() {
         getSettingsUseCase.execute()
-        safeSettingsUseCase.execute()
     }
 }
