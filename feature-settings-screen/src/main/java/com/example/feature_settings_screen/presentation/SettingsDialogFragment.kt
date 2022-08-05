994faf24b48ccf5ee0304233694d3fdfdbfc02a2
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-05 12:50:34 +0300

    Added observe lifedata getSettings

--- feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsDialogFragment.kt
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsDialogFragment.kt
@@ -4,18 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.example.feature_settings_screen.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsDialogFragment : DialogFragment() {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+
     private val vm: SettingsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        vm.settingsLiveData.observe(this@SettingsDialogFragment) { it ->
+            when (it.heightParam) {
+                true -> binding.toggleHeight.check(binding.mHeight.id)
+                else -> binding.toggleHeight.check(binding.ftHeight.id)
+            }
+            when (it.diameterParam) {
+                true -> binding.toggleDiameter.check(binding.mDiameter.id)
+                else -> binding.toggleDiameter.check(binding.ftDiameter.id)
+            }
+            when (it.massParam) {
+                true -> binding.toggleMass.check(binding.kgMass.id)
+                else -> binding.toggleMass.check(binding.lbMass.id)
+            }
+            when (it.payloadParam) {
+                true -> binding.togglePayload.check(binding.kgPayload.id)
+                else -> binding.togglePayload.check(binding.lbPayload.id)
+            }
+        }
     }
 
     override fun onCreateView(
@@ -37,4 +59,27 @@ class SettingsDialogFragment : DialogFragment() {
         super.onDestroyView()
             _binding = null
     }
+
+    override fun onResume() {
+        super.onResume()
+        val listParamSettings = listOf(binding.toggleHeight, binding.toggleDiameter, binding.toggleMass, binding.togglePayload)
+        val listMetricSystem = listOf(binding.mHeight.id, binding.mDiameter.id, binding.kgMass.id, binding.kgPayload.id)
+        binding.textViewClose.setOnClickListener {
+            vm.safe(checkingSettings(listParamSettings, listMetricSystem))
+            dismiss()
+        }
+    }
+
+    private fun checkingSettings(listParamSettings:List<RadioGroup>, listMetricSystem:List<Int>):Map<Int, Boolean> {
+        val mapSettings:MutableMap<Int, Boolean> = mutableMapOf()
+        for (i in listParamSettings.indices) {
+            val paramSettings = listParamSettings[i].checkedRadioButtonId
+            when (listMetricSystem.contains(paramSettings)) {
+                true -> mapSettings[i] = true
+                false -> mapSettings[i] = false
+            }
+        }
+        return mapSettings
+    }
+
 }
