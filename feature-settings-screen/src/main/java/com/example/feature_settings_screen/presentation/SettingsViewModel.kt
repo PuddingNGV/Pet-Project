32ccc3e9ba8c381ad2fe04639f537698e52b773a
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-05 12:49:50 +0300

    Added livedata getSettings

--- feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsViewModel.kt
+++ feature-settings-screen/src/main/java/com/example/feature_settings_screen/presentation/SettingsViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.feature_settings_screen.presentation
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import com.example.feature_settings_screen.domain.usecase.*
@@ -9,12 +11,19 @@ import com.example.feature_settings_screen.domain.models.SettingsParam
 @HiltViewModel
 class SettingsViewModel @Inject constructor(private val getSettingsUseCase: GetSettingsUseCase, private val safeSettingsUseCase: SafeSettingsUseCase) : ViewModel() {
 
-    fun safe() {
-        val settingsParam = SettingsParam(false,false,false,false)
+    val settingsLiveData = load()
+
+    fun safe(mapParamSettings:Map<Int, Boolean>) {
+        val settingsParam = SettingsParam(
+            mapParamSettings.getValue(0),
+            mapParamSettings.getValue(1),
+            mapParamSettings.getValue(2),
+            mapParamSettings.getValue(3)
+        )
         safeSettingsUseCase.execute(settingsParam)
     }
 
-    fun load() {
-        getSettingsUseCase.execute()
+    private fun load(): LiveData<SettingsParam> {
+        return getSettingsUseCase.execute().asLiveData()
     }
 }
