46885da0a9ef4fbe5df41cc36844787c64f8977e
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-08-10 12:38:59 +0300

    Added data class for two parameters

---
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/domain/models/Height.kt
@@ -0,0 +1,6 @@
+package com.example.feature_main_screen.domain.models
+
+data class Height(
+    val feet: Double,
+    val meters: Double
+)
---
+++ feature-main-screen/src/main/java/com/example/feature_main_screen/domain/models/Payload.kt
@@ -0,0 +1,6 @@
+package com.example.feature_main_screen.domain.models
+
+data class Payload(
+    val kg: Int,
+    val lb: Int,
+)
